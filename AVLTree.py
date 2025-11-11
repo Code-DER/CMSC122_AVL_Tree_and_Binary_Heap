e16b75718bc03aab55f3c99f7b4527a4a7d4a771
CODE-Der <xxxcagang@gmail.com>
2025-11-11 15:35:27 +0800

    refactor: transfer code to AVLTree file

---
+++ AVLTree.py
@@ -0,0 +1,16 @@
+class treeNode:
+    def __init__ (self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        self.height = 1
+
+def getHeight(node):
+    if not node:
+        return 0
+    return node.height
+
+def getBalance(node):
+    if not node:
+        return 0
+    return getHeight(node.left) - getHeight(node.right)
