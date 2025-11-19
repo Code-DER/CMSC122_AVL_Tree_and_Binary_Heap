9df28fd250bdd5d601837dbf1bf0673ba4042d29
CODE-Der <xxxcagang@gmail.com>
2025-11-19 11:48:57 +0800

    feat: inOrderTraversal

--- AVLTree.py
+++ AVLTree.py
@@ -62,3 +62,11 @@ def insert(node, data):
         return leftRotate(node)
     
     return node
+
+def inOrderTraversal(node):
+    if node is None:
+        return
+    inOrderTraversal(node.left)
+    print(node.data, end=" ")
+    inOrderTraversal(node.right)
+    
